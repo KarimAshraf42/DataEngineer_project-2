a69357a7836bb97cc214142fea127131ebfaf24a
karim ashraf <karimashraf1309@gmail.com>
2025-10-23 11:38:51 +0300

    second commit

---
+++ ETL/order_payments_trans.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df_order_payments=pd.read_csv('E-Commerce Dataset by Olist _ Dirty/olist_order_payments_dataset.csv')
+
+# print(df_order_payments)
+
+# some informations about order payments table
+# print(df_order_payments.head(10))
+# print('----------------------')
+
+# print(df_order_payments.tail(10))
+# print('----------------------')
+
+# print(df_order_payments.info())
+# print('----------------------')
+
+# print(df_order_payments.isna().sum())
+# print('----------------------')
+
+# print(df_order_payments.describe(include='all'))
+# print('----------------------')
+
+# print(df_order_payments.duplicated().sum())
+# print('----------------------')
+
+# print(df_order_payments['payment_type'].unique())
+# print('----------------------')
+
+# cleaning & transformations
+df_order_payments['order_id']=df_order_payments['order_id'].str.strip()
+df_order_payments['payment_type']=df_order_payments['payment_type'].str.strip().str.lower()
+df_order_payments['payment_type']=df_order_payments['payment_type'].replace(['credit_card','debit_card','not_defined'],['credit card','debit card',np.nan])
+df_order_payments=df_order_payments.dropna(subset=['payment_type'])
+
+df_order_payments['order_id']=df_order_payments['order_id'].astype('string')
+df_order_payments['payment_type']=df_order_payments['payment_type'].astype('category')
+
+# check after cleaning & transformations
+print(df_order_payments.head(10))
+print('----------------------')
+
+# print(df_order_payments.info())
+# print('----------------------')
+
+# print(df_order_payments.describe(include='all'))
+# print('----------------------')
+
+# analysis
+# print('most payment method used')
+# print(df_order_payments['payment_type'].value_counts())
+# print('--------------------------------------------------')
+
+# print('most 10 order had installments')
+# print(df_order_payments.groupby('order_id')['payment_installments'].max().sort_values(ascending=False).head(10))
+# print('--------------------------------------------------')
+
+# print('least 10 order had installments')
+# print(df_order_payments.groupby('order_id')['payment_installments'].min().sort_values(ascending=True).head(10))
+# print('--------------------------------------------------')
+
+df_order_payments.to_csv('order_payments_clean',index=False)
---
+++ ETL/orders_trans.py
@@ -0,0 +1,104 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df_orders=pd.read_csv('E-Commerce Dataset by Olist _ Dirty/olist_orders_dataset.csv')
+
+# print(df_orders)
+
+# some informations about orders table
+# print(df_orders.head(10))
+# print('----------------------')
+
+# print(df_orders.tail(10))
+# print('----------------------')
+
+# print(df_orders.info())
+# print('----------------------')
+
+# print(df_orders.isna().sum())
+# print('----------------------')
+
+# print(df_orders.describe(include='all'))
+# print('----------------------')
+
+# print(df_orders.duplicated().sum())
+# print('----------------------')
+
+# print(df_orders['order_status'].unique())
+# print('----------------------')
+
+# cleaning & transformations
+df_orders['order_id']=df_orders['order_id'].str.strip()
+
+df_orders['customer_id']=df_orders['customer_id'].str.strip()
+
+df_orders['order_status']=df_orders['order_status'].str.strip().str.lower()
+
+df_orders['order_purchase_timestamp']=df_orders['order_purchase_timestamp'].str.strip()
+
+df_orders['order_approved_at']=df_orders['order_approved_at'].str.strip()
+
+df_orders['order_delivered_carrier_date']=df_orders['order_delivered_carrier_date'].str.strip()
+
+df_orders['order_delivered_customer_date']=df_orders['order_delivered_customer_date'].str.strip()
+
+df_orders['order_estimated_delivery_date']=df_orders['order_estimated_delivery_date'].str.strip()
+
+
+df_orders['order_id']=df_orders['order_id'].astype('string')
+
+df_orders['customer_id']=df_orders['customer_id'].astype('string')
+
+df_orders['order_status']=df_orders['order_status'].astype('category')
+
+df_orders['order_purchase_timestamp']=pd.to_datetime(df_orders['order_purchase_timestamp'],format='%Y-%m-%d %H:%M:%S',errors='coerce')
+
+df_orders['order_approved_at']=pd.to_datetime(df_orders['order_approved_at'],format='%Y-%m-%d %H:%M:%S',errors='coerce')
+
+df_orders['order_delivered_carrier_date']=pd.to_datetime(df_orders['order_delivered_carrier_date'],format='%Y-%m-%d %H:%M:%S',errors='coerce')
+
+df_orders['order_delivered_customer_date']=pd.to_datetime(df_orders['order_delivered_customer_date'],format='%Y-%m-%d %H:%M:%S',errors='coerce')
+
+df_orders['order_estimated_delivery_date']=pd.to_datetime(df_orders['order_estimated_delivery_date'],format='%Y-%m-%d %H:%M:%S',errors='coerce')
+
+# check after cleaning & transformations
+print(df_orders.head(10))
+print('----------------------')
+
+# print(df_orders.info())
+# print('----------------------')
+
+# print(df_orders.describe(include='all'))
+# print('----------------------')
+
+# analysis
+print('total orders')
+print(df_orders['order_id'].count())
+print('------------------------------------------------')
+
+print('number for each order status')
+print(df_orders['order_status'].value_counts())
+print('------------------------------------------------')
+
+print('most day have ordered')
+print(df_orders['order_purchase_timestamp'].dt.date.value_counts().head(1))
+print('------------------------------------------------')
+
+print('most delivery time day')
+print((df_orders['order_delivered_customer_date'] - df_orders['order_purchase_timestamp']).dt.days.max())
+print('------------------------------------------------')
+
+print('least delivery time day')
+print((df_orders['order_delivered_customer_date'] - df_orders['order_purchase_timestamp']).dt.days.min())
+print('------------------------------------------------')
+
+print('number of orders that not delivered')
+print(df_orders['order_delivered_customer_date'].isna().sum())
+print('------------------------------------------------')
+
+print('number of orders late')
+print(df_orders[df_orders['order_delivered_customer_date'] > df_orders['order_estimated_delivery_date']].shape[0])
+print('------------------------------------------------')
+
+df_orders.to_csv('orders_clean',index=False)
